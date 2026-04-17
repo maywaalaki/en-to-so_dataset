af518150f61076a5fe86b339f2e7339ce9ef20f8
Laki <lakiup3@gmail.com>
2026-04-17 09:31:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request');
-const SocksProxyAgent = require('socks-proxy-agent'); // Hubi in magacani sax yahay
+const { SocksProxyAgent } = require('socks-proxy-agent'); // HALKAN AYAA LAGA SAXAY {}
 const app = express();
 
 const proxy = 'socks5://127.0.0.1:40001';
@@ -8,9 +8,9 @@ const agent = new SocksProxyAgent(proxy);
 
 app.get('/proxy', (req, res) => {
     const url = req.query.url;
-    if (!url) return res.status(400).send('URL is required');
+    if (!url) return res.status(400).send('No URL provided');
 
     request({ url, agent }).pipe(res);
 });
 
-app.listen(8080, () => console.log('Server is running on port 8080'));
+app.listen(8080, () => console.log('Proxy Bridge Active on Port 8080'));
