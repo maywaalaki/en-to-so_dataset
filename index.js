af7e20427efef8c75ea25e150e52b0329bf79ded
Laki <lakiup3@gmail.com>
2026-04-17 09:18:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request');
-const SocksProxyAgent = require('socks-proxy-agent');
+const SocksProxyAgent = require('socks-proxy-agent'); // Hubi in magacani sax yahay
 const app = express();
 
 const proxy = 'socks5://127.0.0.1:40001';
@@ -8,10 +8,9 @@ const agent = new SocksProxyAgent(proxy);
 
 app.get('/proxy', (req, res) => {
     const url = req.query.url;
-    if (!url) return res.status(400).send('No URL provided');
+    if (!url) return res.status(400).send('URL is required');
 
-    // Tani waxay YouTube u tusaysaa IP-ga Cloudflare
     request({ url, agent }).pipe(res);
 });
 
-app.listen(8000, () => console.log('Proxy Bridge Active on Port 8000'));
+app.listen(8080, () => console.log('Server is running on port 8080'));
